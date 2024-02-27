e3479d6cf2e86ca79c7b5c5c809e4f9d396a50f5
beshoy <said.beshoy@yahoo.com>
2024-02-27 15:35:21 +0200

    lazyLoading-components-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,27 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './share/components/home/home.component';
 import { NotFoundComponent } from './share/components/not-found/not-found.component';
+import { CardComponent } from './share/components/card/card.component';
 
 export const routes: Routes = [
   {
     path: '',
     children: [
       {
-        path: '',
-        component: HomeComponent,
+        path: 'home',
+        loadComponent: () =>
+          import('./share/components/home/home.component').then((m) => {
+            console.log('import home component');
+            return m.HomeComponent;
+          }),
+      },
+      {
+        path: 'card',
+        loadComponent: () =>
+          import('./share/components/card/card.component').then((m) => {
+            console.log('import card component');
+            return m.CardComponent;
+          }),
       },
     ],
   },
